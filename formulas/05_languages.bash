659be6da5e11f7e2ad60441772cc91815064c9a4
jimmynotjim <jimmynotjim@me.com>
2013-12-08 20:19:56 -0500

    Removed non-brew commands

--- formulas/05_languages.bash
+++ formulas/05_languages.bash
@@ -38,16 +38,6 @@ unlink  node08
 # ocaml
 #install ocaml
 
-# php
-install php55 --without-pear --without-apache --with-pgsql --with-fpm --with-intl
-unlink  php55
-
-install php54 --without-pear --without-apache --with-pgsql --with-fpm --with-intl
-unlink  php54
-
-install php53 --without-pear --without-apache --with-pgsql --with-fpm --with-intl
-unlink  php53
-
 # python
 install python
 
@@ -55,12 +45,6 @@ install python
 install chruby
 
 install ruby-install
-
-ruby-install ruby
-ruby-install ruby 1.9.2
-ruby-install ruby 1.9.3
-
-chruby ruby-2.0
 
 # rust
 #install rust
