3887aea80407dc30e262b1ae8dccf77a0989d8e5
James Wilson <jimmynotjim@me.com>
2018-02-06 09:02:06 -0600

    Update MAS list for missing apps

--- formulas/11_mas.bash
+++ formulas/11_mas.bash
@@ -6,6 +6,17 @@ install mas
 
 mas 'Numbers', id: 409203825
 mas 'Bear', id: 1091189122
+mas 'Byword', id: 420212497
+mas 'Disk Doctor: System Cleaner', id: 455970963
+mas 'Icon Slate', id: 439697913
+mas 'iTeleport: VNC & RDP', id: 489094439
+mas 'Keynote', id: 409183694
+mas 'Memory Clean 3: Free Up Memory', id: 1302310792
+mas 'Pages', id: 409201541
+mas 'Patterns - The Regex App', id: 429449079
+mas 'Slack', id: 803453959
 mas 'Spark - Love your email again', id: 1176895641
 mas 'Shush - Microphone Manager', id: 496437906
 mas 'Pixelmator', id: 407963104
+mas 'Tweetbot for Twitter', id: 557168941
+mas 'The Unarchiver', id: 425424353
